d1e1991f62669c3ad4cc84524c67644925d05742
Keith Flynn <flynn.keith@gmail.com>
2023-10-17 17:08:04 -0400

    Add localStorage allowing persistant lists
    Add function to save list items to localstorage and recall upon loading.
     Typescript cold pricklies also persist, but are next on the docket.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import NewTodoForm from "./NewTodoForm";
 import TodoList from "./TodoList";
 import "./styles.css";
@@ -10,7 +10,17 @@ type TodoItem = {
 };
 
 function App() {
-  const [todos, setTodos] = useState<TodoItem[]>([]);
+  //const [todos, setTodos] = useState<TodoItem[]>([]);
+  const [todos, setTodos] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+
+    return JSON.parse(localValue)
+  })
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(todos))
+  }, [todos])
 
   function addTodo(title: string) {
     setTodos((currentTodos) => {
